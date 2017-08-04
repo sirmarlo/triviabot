1b56f18fe1474f1bbf988f6f4ad9d95ac02070e4
Marlo Epres <marlo.epres@mlb.com>
2017-08-04 15:40:43 -0400

    add POST support

--- triviabot.py
+++ triviabot.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def main():
 	return "<html><a href='http://gph.is/2w17nf7'/></html>"
 
-@app.route('/scores')
+@app.route('/scores', methods=['GET', 'POST'])
 def scores():
 	return "Dorville - 14, Dan - 8, Marlo - 7, Matt - 1, Achintya - 1"
